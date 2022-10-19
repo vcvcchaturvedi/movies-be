49e9ec9b787216d4a913b15feb2fc67487904a0d
Vinay Chaturvedi <vcvcchaturvedi@gmail.com>
2022-10-19 14:22:34 +0530

    added check console logs

--- pool.js
+++ pool.js
@@ -2,7 +2,6 @@ const mysql = require("mysql");
 const dotenv = require("dotenv");
 dotenv.config();
 const pool = mysql.createPool({
-  connectionLimit: 2,
   user: "root",
   password: process.env.PASSWORD,
   database: "movies",
