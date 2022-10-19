06cec00ac628b2817b0b65ac42379b996cc2a9b2
Vinay Chaturvedi <vcvcchaturvedi@gmail.com>
2022-10-19 14:49:43 +0530

    added check console logs

--- pool.js
+++ pool.js
@@ -3,8 +3,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 const pool = mysql.createPool({
   connectTimeout: 60 * 1000,
-  acquireTimeout: 60 * 1000,
-  timeout: 60 * 1000,
   waitForConnections: true,
   user: "root",
   password: process.env.PASSWORD,
