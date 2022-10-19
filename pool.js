d1d3550a246211309f56743520777ab946348a55
Vinay Chaturvedi <vcvcchaturvedi@gmail.com>
2022-10-19 08:25:02 +0530

    Changed db connection config

--- pool.js
+++ pool.js
@@ -1,9 +1,11 @@
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
 const pool = mysql.createPool({
-  connectionLimit: 2,
-  host: process.env.HOST,
-  user: "root",
-  password: process.env.PASSWORD,
-  database: "movies",
+  connectionLimit: 1,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.INSTANCE_HOST,
 });
 module.exports = { pool };
